79b2c4672dbe40f56f05c27a4bf23b4e6b43fbd2
Hugo Cruz <hugocruzlfc@gmail.com>
2025-04-23 17:15:34 +0200

    feat: add routines pagination

--- api/routers/routines.py
+++ api/routers/routines.py
@@ -16,10 +16,55 @@ class RoutineBase(BaseModel):
     
 class RoutineCreate(RoutineBase):
     workouts: List[int] = []
+
+class RoutineResponse(BaseModel):
+    id: int
+    user_id: int
+    name: str
+    description: str
+    workouts: List[int]
+  
+
+class RoutinesPageResponse(BaseModel):
+    routines: List[RoutineResponse]
+    previousCursor: Optional[str]
     
-@router.get("/")
-def get_routines(db: db_dependency, user: user_dependency):
-    return db.query(Routine).options(joinedload(Routine.workouts)).filter(Routine.user_id == user.get('id')).all()
+@router.get("/",response_model=RoutinesPageResponse)
+def get_routines(db: db_dependency, user: user_dependency,cursor: Optional[str] = None,limit: int = 10):
+    query = (
+        db.query(Routine)
+        .options(joinedload(Routine.workouts))
+        .filter(Routine.user_id == user.get("id"))
+    )
+
+    if cursor:
+        query = query.filter(Routine.id < int(cursor))
+
+    routines = (
+        query.order_by(Routine.id.desc()) 
+        .limit(limit)
+        .all()
+    )
+
+    previous_cursor = None
+    if routines and len(routines) == limit:
+        previous_cursor = str(routines[-1].id)
+
+    routines_response = [
+        RoutineResponse(
+            id=routine.id,
+            user_id=routine.user_id,
+            name=routine.name,
+            description=routine.description,
+            workouts=[workout.id for workout in routine.workouts]
+        )
+        for routine in routines
+    ]
+
+    return {
+        "routines": routines_response,
+        "previousCursor": previous_cursor
+    }
 
 @router.post("/")
 def create_routine(db: db_dependency, user: user_dependency, routine: RoutineCreate):
